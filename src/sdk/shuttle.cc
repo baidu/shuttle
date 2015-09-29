59b8a830b4a1fb7a6fb0cd176dca077f59093095
Kai-Zhang <cs.zhangkai@outlook.com>
2015-09-29 16:55:50 +0800

    Implement client functions

--- src/sdk/shuttle.cc
+++ src/sdk/shuttle.cc
@@ -13,14 +13,16 @@ class ShuttleImpl : public Shuttle {
 public:
     ShuttleImpl(const std::string& master_addr);
     virtual ~ShuttleImpl();
-    bool SubmitJob(const sdk::JobDescription& job_desc, std::string* job_id);
-    bool UpdateJob(const std::string& job_id, 
-                   const sdk::JobDescription& job_desc);
+    bool SubmitJob(const sdk::JobDescription& job_desc, std::string& job_id);
+    bool UpdateJob(const std::string& job_id,
+                   const sdk::JobPriority& priority = sdk::kUndefined,
+                   const int map_capacity = -1,
+                   const int reduce_capacity = -1);
     bool KillJob(const std::string& job_id);
     bool ShowJob(const std::string& job_id, 
-                 sdk::JobInstance* job,
-                 std::vector<sdk::TaskInstance>* tasks);
-    bool ListJobs(std::vector<sdk::JobInstance>* jobs);
+                 sdk::JobInstance& job,
+                 std::vector<sdk::TaskInstance>& tasks);
+    bool ListJobs(std::vector<sdk::JobInstance>& jobs);
 private:
     std::string master_addr_;
     Master_Stub* master_stub_;
@@ -40,13 +42,14 @@ ShuttleImpl::~ShuttleImpl() {
     delete master_stub_;
 }
 
-bool ShuttleImpl::SubmitJob(const sdk::JobDescription& job_desc, std::string* job_id) {
+bool ShuttleImpl::SubmitJob(const sdk::JobDescription& job_desc, std::string& job_id) {
     ::baidu::shuttle::SubmitJobRequest request;
     ::baidu::shuttle::SubmitJobResponse response;
     ::baidu::shuttle::JobDescriptor* job = request.mutable_job();
     job->set_name(job_desc.name);
     job->set_user(job_desc.user);
-    job->set_priority((JobPriority)job_desc.priority);
+    job->set_priority((job_desc.priority == sdk::kUndefined) ?
+            kNormal : (JobPriority)job_desc.priority);
     job->set_map_capacity(job_desc.map_capacity);
     job->set_reduce_capacity(job_desc.reduce_capacity);
     job->set_millicores(job_desc.millicores);
@@ -72,20 +75,24 @@ bool ShuttleImpl::SubmitJob(const sdk::JobDescription& job_desc, std::string* jo
     if (response.status() != kOk) {
         return false;
     }
-    if (job_id != NULL) {
-        *job_id = response.jobid();
-    }
+    job_id = response.jobid();
     return true;
 }
 
-bool ShuttleImpl::UpdateJob(const std::string& job_id, 
-                            const sdk::JobDescription& job_desc) {
+bool ShuttleImpl::UpdateJob(const std::string& job_id, const sdk::JobPriority& priority,
+                            const int map_capacity, const int reduce_capacity) {
     ::baidu::shuttle::UpdateJobRequest request;
     ::baidu::shuttle::UpdateJobResponse response;
     request.set_jobid(job_id);
-    request.set_priority((JobPriority)job_desc.priority);
-    request.set_map_capacity(job_desc.map_capacity);
-    request.set_reduce_capacity(job_desc.reduce_capacity);
+    if (priority != sdk::kUndefined) {
+        request.set_priority((JobPriority)priority);
+    }
+    if (map_capacity != -1) {
+        request.set_map_capacity(map_capacity);
+    }
+    if (reduce_capacity != -1) {
+        request.set_reduce_capacity(reduce_capacity);
+    }
 
     bool ok = rpc_client_.SendRequest(master_stub_, &Master_Stub::UpdateJob,
                                       &request, &response, 2, 1);
@@ -111,8 +118,8 @@ bool ShuttleImpl::KillJob(const std::string& job_id) {
 }
 
 bool ShuttleImpl::ShowJob(const std::string& job_id, 
-                          sdk::JobInstance* job,
-                          std::vector<sdk::TaskInstance>* tasks) {
+                          sdk::JobInstance& job,
+                          std::vector<sdk::TaskInstance>& tasks) {
     ::baidu::shuttle::ShowJobRequest request;
     ::baidu::shuttle::ShowJobResponse response;
     request.set_jobid(job_id);
@@ -126,61 +133,59 @@ bool ShuttleImpl::ShowJob(const std::string& job_id,
     if (response.status() != kOk) {
         return false;
     }
-    if (job != NULL) {
-        const JobOverview& joboverview = response.job();
-        const JobDescriptor& desc = joboverview.desc();
-        job->desc.name = desc.name();
-        job->desc.user = desc.user();
-        job->desc.priority = (sdk::JobPriority)desc.priority();
-        job->desc.map_capacity = desc.map_capacity();
-        job->desc.reduce_capacity = desc.reduce_capacity();
-        job->desc.millicores = desc.millicores();
-        job->desc.memory = desc.memory();
-        std::copy(desc.inputs().begin(), desc.inputs().end(),
-                  std::back_inserter(job->desc.inputs));
-        job->desc.output = desc.output();
-        std::copy(desc.files().begin(), desc.files().end(),
-                  std::back_inserter(job->desc.files));
-        job->desc.map_command = desc.map_command();
-        job->desc.reduce_command = desc.reduce_command();
 
-        job->jobid = joboverview.jobid();
-        job->state = (sdk::JobState)joboverview.state();
+    const JobOverview& joboverview = response.job();
+    const JobDescriptor& desc = joboverview.desc();
+    job.desc.name = desc.name();
+    job.desc.user = desc.user();
+    job.desc.priority = (sdk::JobPriority)desc.priority();
+    job.desc.map_capacity = desc.map_capacity();
+    job.desc.reduce_capacity = desc.reduce_capacity();
+    job.desc.millicores = desc.millicores();
+    job.desc.memory = desc.memory();
+    std::copy(desc.inputs().begin(), desc.inputs().end(),
+              std::back_inserter(job.desc.inputs));
+    job.desc.output = desc.output();
+    std::copy(desc.files().begin(), desc.files().end(),
+              std::back_inserter(job.desc.files));
+    job.desc.map_command = desc.map_command();
+    job.desc.reduce_command = desc.reduce_command();
 
-        const TaskStatistics& map_stat = joboverview.map_stat();
-        job->map_stat.total = map_stat.total();
-        job->map_stat.pending = map_stat.pending();
-        job->map_stat.running = map_stat.running();
-        job->map_stat.failed = map_stat.failed();
-        job->map_stat.killed = map_stat.killed();
-        job->map_stat.completed = map_stat.completed();
+    job.jobid = joboverview.jobid();
+    job.state = (sdk::JobState)joboverview.state();
 
-        const TaskStatistics& reduce_stat = joboverview.reduce_stat();
-        job->reduce_stat.total = reduce_stat.total();
-        job->reduce_stat.pending = reduce_stat.pending();
-        job->reduce_stat.running = reduce_stat.running();
-        job->reduce_stat.failed = reduce_stat.failed();
-        job->reduce_stat.killed = reduce_stat.killed();
-        job->reduce_stat.completed = reduce_stat.completed();
-    }
-    if (tasks != NULL) {
-        ::google::protobuf::RepeatedPtrField<TaskOverview>::const_iterator it;
-        for (it = response.tasks().begin(); it != response.tasks().end(); ++it) {
-            sdk::TaskInstance task;
-            const TaskInfo& info = it->info();
-            task.job_id = info.task_id();
-            task.attempt_id = info.attempt_id();
-            task.input_file = info.input().input_file();
-            task.state = (sdk::TaskState)it->state();
-            task.minion_addr = it->minion_addr();
-            task.progress = it->progress();
-            tasks->push_back(task);
-        }
+    const TaskStatistics& map_stat = joboverview.map_stat();
+    job.map_stat.total = map_stat.total();
+    job.map_stat.pending = map_stat.pending();
+    job.map_stat.running = map_stat.running();
+    job.map_stat.failed = map_stat.failed();
+    job.map_stat.killed = map_stat.killed();
+    job.map_stat.completed = map_stat.completed();
+
+    const TaskStatistics& reduce_stat = joboverview.reduce_stat();
+    job.reduce_stat.total = reduce_stat.total();
+    job.reduce_stat.pending = reduce_stat.pending();
+    job.reduce_stat.running = reduce_stat.running();
+    job.reduce_stat.failed = reduce_stat.failed();
+    job.reduce_stat.killed = reduce_stat.killed();
+    job.reduce_stat.completed = reduce_stat.completed();
+
+    ::google::protobuf::RepeatedPtrField<TaskOverview>::const_iterator it;
+    for (it = response.tasks().begin(); it != response.tasks().end(); ++it) {
+        sdk::TaskInstance task;
+        const TaskInfo& info = it->info();
+        task.job_id = info.task_id();
+        task.attempt_id = info.attempt_id();
+        task.input_file = info.input().input_file();
+        task.state = (sdk::TaskState)it->state();
+        task.minion_addr = it->minion_addr();
+        task.progress = it->progress();
+        tasks.push_back(task);
     }
     return true;
 }
 
-bool ShuttleImpl::ListJobs(std::vector<sdk::JobInstance>* jobs) {
+bool ShuttleImpl::ListJobs(std::vector<sdk::JobInstance>& jobs) {
     ::baidu::shuttle::ListJobsRequest request;
     ::baidu::shuttle::ListJobsResponse response;
 
@@ -193,46 +198,44 @@ bool ShuttleImpl::ListJobs(std::vector<sdk::JobInstance>* jobs) {
     if (response.status() != kOk) {
         return false;
     }
-    if (jobs != NULL) {
-        ::google::protobuf::RepeatedPtrField<JobOverview>::const_iterator it;
-        for (it = response.jobs().begin(); it != response.jobs().end(); ++it) {
-            sdk::JobInstance job;
-            const JobDescriptor& desc = it->desc();
-            job.desc.name = desc.name();
-            job.desc.user = desc.user();
-            job.desc.priority = (sdk::JobPriority)desc.priority();
-            job.desc.map_capacity = desc.map_capacity();
-            job.desc.reduce_capacity = desc.reduce_capacity();
-            job.desc.millicores = desc.millicores();
-            job.desc.memory = desc.memory();
-            std::copy(desc.inputs().begin(), desc.inputs().end(),
-                      std::back_inserter(job.desc.inputs));
-            job.desc.output = desc.output();
-            std::copy(desc.files().begin(), desc.files().end(),
-                      std::back_inserter(job.desc.files));
-            job.desc.map_command = desc.map_command();
-            job.desc.reduce_command = desc.reduce_command();
+    ::google::protobuf::RepeatedPtrField<JobOverview>::const_iterator it;
+    for (it = response.jobs().begin(); it != response.jobs().end(); ++it) {
+        sdk::JobInstance job;
+        const JobDescriptor& desc = it->desc();
+        job.desc.name = desc.name();
+        job.desc.user = desc.user();
+        job.desc.priority = (sdk::JobPriority)desc.priority();
+        job.desc.map_capacity = desc.map_capacity();
+        job.desc.reduce_capacity = desc.reduce_capacity();
+        job.desc.millicores = desc.millicores();
+        job.desc.memory = desc.memory();
+        std::copy(desc.inputs().begin(), desc.inputs().end(),
+                  std::back_inserter(job.desc.inputs));
+        job.desc.output = desc.output();
+        std::copy(desc.files().begin(), desc.files().end(),
+                  std::back_inserter(job.desc.files));
+        job.desc.map_command = desc.map_command();
+        job.desc.reduce_command = desc.reduce_command();
 
-            job.jobid = it->jobid();
-            job.state = (sdk::JobState)it->state();
+        job.jobid = it->jobid();
+        job.state = (sdk::JobState)it->state();
 
-            const TaskStatistics& map_stat = it->map_stat();
-            job.map_stat.total = map_stat.total();
-            job.map_stat.pending = map_stat.pending();
-            job.map_stat.running = map_stat.running();
-            job.map_stat.failed = map_stat.failed();
-            job.map_stat.killed = map_stat.killed();
-            job.map_stat.completed = map_stat.completed();
+        const TaskStatistics& map_stat = it->map_stat();
+        job.map_stat.total = map_stat.total();
+        job.map_stat.pending = map_stat.pending();
+        job.map_stat.running = map_stat.running();
+        job.map_stat.failed = map_stat.failed();
+        job.map_stat.killed = map_stat.killed();
+        job.map_stat.completed = map_stat.completed();
 
-            const TaskStatistics& reduce_stat = it->reduce_stat();
-            job.reduce_stat.total = reduce_stat.total();
-            job.reduce_stat.pending = reduce_stat.pending();
-            job.reduce_stat.running = reduce_stat.running();
-            job.reduce_stat.failed = reduce_stat.failed();
-            job.reduce_stat.killed = reduce_stat.killed();
-            job.reduce_stat.completed = reduce_stat.completed();
-            jobs->push_back(job);
-        }
+        const TaskStatistics& reduce_stat = it->reduce_stat();
+        job.reduce_stat.total = reduce_stat.total();
+        job.reduce_stat.pending = reduce_stat.pending();
+        job.reduce_stat.running = reduce_stat.running();
+        job.reduce_stat.failed = reduce_stat.failed();
+        job.reduce_stat.killed = reduce_stat.killed();
+        job.reduce_stat.completed = reduce_stat.completed();
+        jobs.push_back(job);
     }
     return true;
 }
