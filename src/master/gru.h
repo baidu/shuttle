e87e4d28dac5be3e30975f66c485af190ec06833
Kai-Zhang <cs.zhangkai@outlook.com>
2015-12-03 01:25:30 +0800

    Add some general members in Gru interface

--- src/master/gru.h
+++ src/master/gru.h
@@ -1,7 +1,12 @@
 #ifndef _BAIDU_SHUTTLE_GRU_H_
 #define _BAIDU_SHUTTLE_GRU_H_
 #include <string>
+#include <vector>
+#include <queue>
+#include "galaxy_handler.h"
 #include "proto/shuttle.pb.h"
+#include "mutex.h"
+#include "thread_pool.h"
 
 namespace baidu {
 namespace shuttle {
@@ -27,10 +32,27 @@ struct AllocateItemComparator {
     }
 };
 
+class RpcClient;
+
 // Interface Gru
 template <class Resource>
 class Gru {
 public:
+    // General initialization
+    Gru(const JobDescriptor& job, int node) :
+            manager_(NULL), rpc_client_(NULL), job_(job), galaxy_(NULL),
+            node_(node), total_tasks_(0), start_time_(0), end_time_(0),
+            killed_(0), failed_(0), end_game_begin_(0), monitor_(1) {
+        rpc_client_ = new RpcClient();
+        start_time_ = std::time(NULL);
+        end_time_ = start_time_ - 1;
+    }
+    virtual ~Gru() {
+        if (rpc_client_ != NULL) {
+            delete rpc_client_;
+            rpc_client_ = NULL;
+        }
+    }
     // Operations
     virtual Status Start() = 0;
     virtual Status Update(const std::string& priority, int capacity) = 0;
@@ -39,14 +61,73 @@ public:
     virtual Status Finish(int no, int attempt, TaskState state) = 0;
 
     // Data getters
-    virtual time_t GetStartTime() const = 0;
-    virtual time_t GetFinishTime() const = 0;
-    virtual TaskStatistics GetStatistics() const = 0;
     virtual Status GetHistory(const std::vector& buf) const = 0;
+
+    // Non-interface part
+    virtual time_t GetStartTime() const {
+        // start_time_ generated at beginning and is read-only ever since
+        return start_time_;
+    }
+    virtual time_t GetFinishTime() const {
+        MutexLock lock(&meta_mu_);
+        return end_time_;
+    }
+    virtual TaskStatistics GetStatistics() const {
+        int pending = 0, running = 0, completed = 0;
+        if (manager_ != NULL) {
+            pending = manager->Pending();
+            running = manager->Allocated();
+            completed = manager->Done();
+        }
+        MutexLock lock(&meta_mu_);
+        TaskStatistics task;
+        task.set_total(total_tasks_);
+        task.set_pending(pending);
+        task.set_running(running);
+        task.set_failed(failed_);
+        task.set_killed(killed_);
+        task.set_completed(completed);
+        return task;
+    }
 
     // For backup and recovery
     // Load()
     // Dump()
+
+protected:
+    // TODO template don't support outside definition.
+    // so maybe consider a new method, since this monitor
+    // can be all the same in all grus.
+    void KeepMonitoring();
+
+protected:
+    BasicResourceManager<Resource>* manager_;
+    // Carefully initialized
+    RpcClient* rpc_client_;
+    // Carefully initialized
+    const JobDescriptor& job_;
+    Mutex meta_mu_;
+    // Initialized to NULL since every gru differs in galaxy
+    GalaxyHandler* galaxy_;
+    // Carefully initialized
+    int node_;
+    // Initialized to 0 since it depends on the gru
+    int total_tasks_;
+    // Carefully initialized
+    time_t start_time_;
+    // Carefully initialized
+    time_t end_time_;
+    int killed_;
+    int failed_;
+    int end_game_begin_;
+
+    Mutex alloc_mu_;
+    ThreadPool monitor_;
+    std::queue<int> slugs_;
+    std::vector<int> failed_count;
+    std::vector<AllocateItem*> allocation_table_;
+    std::priority_queue<AllocateItem*, std::vector<AllocateItem*>,
+                        AllocateItemComparator> time_heap_;
 };
 
 class AlphaGru : public Gru<ResourceItem> {
@@ -59,11 +140,7 @@ public:
     virtual Status Finish(int no, int attempt, TaskState state);
 
     // Data getters
-    virtual time_t GetStartTime() const;
-    virtual time_t GetFinishTime() const;
-    virtual TaskStatistics GetStatistics() const;
     virtual Status GetHistory(const std::vector& buf) const;
-
 };
 
 class BetaGru : public Gru<IdItem> {
@@ -76,9 +153,6 @@ public:
     virtual Status Finish(int no, int attempt, TaskState state);
 
     // Data getters
-    virtual time_t GetStartTime() const;
-    virtual time_t GetFinishTime() const;
-    virtual TaskStatistics GetStatistics() const;
     virtual Status GetHistory(const std::vector& buf) const;
 };
 
@@ -92,9 +166,6 @@ public:
     virtual Status Finish(int no, int attempt, TaskState state);
 
     // Data getters
-    virtual time_t GetStartTime() const;
-    virtual time_t GetFinishTime() const;
-    virtual TaskStatistics GetStatistics() const;
     virtual Status GetHistory(const std::vector& buf) const;
 };
 
