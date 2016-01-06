441610b32782c9010eeafd2d721331267ed14139
fxsjy <ccnusjy@gmail.com>
2016-01-06 14:53:07 +0800

    use old style galaxy SDK

--- build_galaxy.sh
+++ build_galaxy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-git clone https://github.com/bluebore/galaxy
+git clone https://github.com/fxsjy/galaxy
 if [ $? -ne - ]; then
 	echo "faild to clone galaxy reposiotry"
 	exit -1
 fi
+(cd galaxy && git checkout old_sdk_for_shuttle)
 cd galaxy && sh -x ./build4internal.sh
 if [ $? -ne 0 ]; then
 	echo "build depends of galaxy failed"
