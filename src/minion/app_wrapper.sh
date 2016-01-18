e36b10fad0389fc923d9b04936a6c562a7dfa50f
fxsjy <ccnusjy@gmail.com>
2016-01-18 09:44:38 +0800

    improve app_wrapper

--- src/minion/app_wrapper.sh
+++ src/minion/app_wrapper.sh
@@ -3,6 +3,15 @@ set -x
 set -o pipefail
 user_cmd=$*
 
+PsTree() {
+    echo "$1"
+    local children=$(ps -o pid= --ppid "$1") 
+    for pid in $children
+    do
+            PsTree "$pid"
+    done
+}
+
 JailRun() {
 	set -x
 	ulimit -m ${mapred_memory_limit}
@@ -11,11 +20,35 @@ JailRun() {
 	return $?
 }
 
+InputRun() {
+	input_cmd=$*
+	eval $input_cmd
+	local ret=$?
+	if [ $ret -ne 0 ]; then
+		kill -9 $(PsTree $$)
+		return $ret
+	fi
+	return 0
+}
+
+ShuffleRun() {
+	shuffle_cmd=$*
+	eval $shuffle_cmd
+	local ret=$?
+	if [ $ret -ne 0 ]; then
+		kill -9 $(PsTree $$)
+		return $ret
+	fi
+	return 0
+}
+
 if [ "${mapred_task_is_map}" == "true" ]
 then
 	work_dir="map_${mapred_task_partition}_${mapred_attempt_id}"
-	mkdir $work_dir
-	cd $work_dir
+	mkdir $work_dir && cd $work_dir
+	if [ $? -ne 0 ]; then
+		exit -2
+	fi
 	cat ../common.list | while read f_name; do ln -s ../$f_name .; done
 
 	dfs_flags=""
@@ -37,15 +70,18 @@ then
 	if [ "${minion_input_is_nline}" == "true" ] ; then
 		is_nline="-is_nline"
 	fi
-	./input_tool -file=${map_input_file} \
+	input_cmd="./input_tool -file=${map_input_file} \
 	-offset=${map_input_start} \
-	-len=${map_input_length} ${dfs_flags} ${format} ${pipe_style} ${is_nline} | JailRun 2>./stderr
+	-len=${map_input_length} ${dfs_flags} ${format} ${pipe_style} ${is_nline}"
+	(InputRun $input_cmd | JailRun) 2>./stderr
 	exit $?
 elif [ "${mapred_task_is_map}" == "false" ]
 then
 	work_dir="reduce_${mapred_task_partition}_${mapred_attempt_id}"
-	mkdir $work_dir
-	cd $work_dir
+	mkdir $work_dir && cd $work_dir
+	if [ $? -ne 0 ]; then
+		exit -2
+	fi
 	cat ../common.list | while read f_name; do ln -s ../$f_name .; done
 
 	dfs_flags=""
@@ -59,10 +95,11 @@ then
 	if [ "${minion_pipe_style}" != "" ]; then
 		pipe_style="-pipe ${minion_pipe_style}"
 	fi
-	./shuffle_tool -total=${mapred_map_tasks} \
+	shuffle_cmd="./shuffle_tool -total=${mapred_map_tasks} \
 	-work_dir=${minion_shuffle_work_dir} \
 	-reduce_no=${mapred_task_partition} \
-	-attempt_id=${mapred_attempt_id} $dfs_flags $pipe_style | JailRun 2>./stderr
+	-attempt_id=${mapred_attempt_id} $dfs_flags $pipe_style"
+	(ShuffleRun $shuffle_cmd | JailRun) 2>./stderr
 	exit $?
 else
 	echo "not in shuttle env"
