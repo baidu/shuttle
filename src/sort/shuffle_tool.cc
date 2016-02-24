090f8f4002f5b49f508124642298153fbac8870f
fxsjy <ccnusjy@gmail.com>
2016-02-24 16:57:38 +0800

    less footprint on dfs

--- src/sort/shuffle_tool.cc
+++ src/sort/shuffle_tool.cc
@@ -187,6 +187,7 @@ bool MergeOneTuo(int map_from, int map_to, int tuo_now) {
     ss << FLAGS_work_dir << "/" << tuo_now << ".tuo";
     const std::string real_tuo_name = ss.str();
     if (!g_fs->Rename(output_file, real_tuo_name)) {
+        g_fs->Remove(output_file);
         return false;
     }
     std::vector<std::string>::iterator it;
@@ -221,7 +222,7 @@ int MergeTuo() {
                 LOG(INFO, "lucky, total #%d/%d tuo ready", ready_tuo_set.size(), n_tuo);
                 continue;
             }
-            if (FLAGS_reduce_no < 3 * n_tuo) {
+            if (FLAGS_reduce_no <= n_tuo) {
                 int map_from = tuo_now * FLAGS_tuo_size;
                 int map_to = std::min( (tuo_now + 1) * FLAGS_tuo_size - 1, FLAGS_total - 1);
                 LOG(INFO, "merge tuo from %d to %d", map_from, map_to);
