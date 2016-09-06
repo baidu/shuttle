026267c8abb19b90ebbc7feee6034b241444d4fa
Kai-Zhang <cs.zhangkai@outlook.com>
2016-09-06 16:32:04 +0800

    Rewrite testcases for partitioner

--- src/test/partition.test.cc
+++ src/test/partition.test.cc
@@ -8,78 +8,143 @@
 
 using namespace baidu::shuttle;
 
-TEST(Partitioner, KeyBased) {
-    TaskInfo task;
-    task.mutable_job()->set_reduce_total(10);
-    task.mutable_job()->set_key_separator(" ");
-    task.mutable_job()->set_key_fields_num(2);
-    task.mutable_job()->set_partition_fields_num(1);
-    KeyFieldBasedPartitioner kf_parti(task);
+/*
+ * Test will run without any parameters
+ */
+
+const int dest_num = 100;
+
+TEST(KeyBasedPartitionerTest, NormalTest) {
+    NodeConfig config;
+    // Following three attributes is needed
+    config.set_key_separator(" ");
+    config.set_key_fields_num(2);
+    config.set_partition_fields_num(1);
+
+    Partitioner* partitioner = Partitioner::Get(
+            kKeyFieldBasedPartitioner, config, dest_num);
+
     std::string key;
-    int reduce_no = kf_parti.Calc("abc 555 zzzzz", &key);
-    EXPECT_EQ(key, "abc 555");
-    EXPECT_EQ(reduce_no, kf_parti.HashCode("abc") % 10);
-    reduce_no = kf_parti.Calc("sjy", &key);
-    EXPECT_EQ(key, "sjy");
-    EXPECT_EQ(reduce_no, kf_parti.HashCode("sjy") % 10);
-    reduce_no = kf_parti.Calc("", &key);
+    // Normal case
+    int dest = partitioner->Calc("key1 key2 value", &key);
+    EXPECT_EQ(key, "key1 key2");
+    EXPECT_EQ(dest, partitioner->HashCode("key1") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("key1"));
+    // Column is less than key fields number
+    dest = partitioner->Calc("line", &key);
+    EXPECT_EQ(key, "line");
+    EXPECT_EQ(dest, partitioner->HashCode("line") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("line"));
+    // Empty string
+    dest = partitioner->Calc("", &key);
     EXPECT_EQ(key, "");
-    EXPECT_EQ(reduce_no, 0);
+    EXPECT_EQ(dest, 0);
+    EXPECT_EQ(dest, partitioner->Calc(""));
+    // No such separator
+    dest = partitioner->Calc("key1\tkey2", &key);
+    EXPECT_EQ(key, "key1\tkey2");
+    EXPECT_EQ(dest, partitioner->HashCode("key1\tkey2") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("key1\tkey2"));
+
+    delete partitioner;
 }
 
+TEST(KeyBasedPartitionerTest, HighFieldsTest) {
+    NodeConfig config;
+    config.set_key_separator(" ");
+    config.set_key_fields_num(2);
+    config.set_partition_fields_num(3);
 
-TEST(Partitioner, KeyBasedInvalid) {
-    TaskInfo task;
-    task.mutable_job()->set_reduce_total(100);
-    task.mutable_job()->set_key_separator("\t");
-    task.mutable_job()->set_key_fields_num(3);
-    task.mutable_job()->set_partition_fields_num(1);
-    KeyFieldBasedPartitioner kf_parti(task);
+    Partitioner* partitioner = Partitioner::Get(
+            kKeyFieldBasedPartitioner, config, dest_num);
+
     std::string key;
-    int reduce_no = kf_parti.Calc("k1\tk2", &key);
-    EXPECT_EQ(key, "k1\tk2");
-    EXPECT_EQ(reduce_no, kf_parti.HashCode("k1") % 100);
+    // Exact case
+    int dest = partitioner->Calc("key1 key2 key3 value", &key);
+    EXPECT_EQ(key, "key1 key2");
+    EXPECT_EQ(dest, partitioner->HashCode("key1 key2 key3") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("key1 key2 key3"));
+    // Column is less than partition fields number
+    dest = partitioner->Calc("col1 col2", &key);
+    EXPECT_EQ(key, "col1 col2");
+    EXPECT_EQ(dest, partitioner->HashCode("col1 col2") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("col1 col2"));
+
+    delete partitioner;
 }
 
-TEST(Partitioner, KeyDefault) {
-    TaskInfo task;
-    task.mutable_job()->set_reduce_total(100);
-    KeyFieldBasedPartitioner kf_parti(task);
+TEST(PartitionerTest, DefaultValueTest) {
+    NodeConfig config;
+
+    Partitioner* partitioner = Partitioner::Get(
+            kKeyFieldBasedPartitioner, config, dest_num);
+
     std::string key;
-    int reduce_no = kf_parti.Calc("k1\tk2", &key);
-    EXPECT_EQ(key, "k1");
-    EXPECT_EQ(reduce_no, kf_parti.HashCode("k1") % 100);
+    // Default separator: \t
+    int dest = partitioner->Calc("key1\tkey2", &key);
+    EXPECT_EQ(key, "key1");
+    EXPECT_EQ(dest, partitioner->HashCode("key1") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("key1"));
+    // Default key fields: 1, partition fields: 1
+    dest = partitioner->Calc("col1\tcol2\tcol3\tcol4", &key);
+    EXPECT_EQ(key, "col1");
+    EXPECT_EQ(dest, partitioner->HashCode("col1") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("col1"));
+
+    delete partitioner;
 }
 
-TEST(Partitioner, IntHash) {
-    TaskInfo task;
-    task.mutable_job()->set_reduce_total(100);
-    task.mutable_job()->set_key_separator(" ");
-    IntHashPartitioner ih_parti(task);
-    std::string key;
-    int reduce_no = ih_parti.Calc("17 key_123 value456", &key);
-    EXPECT_EQ(key, "key_123");
-    EXPECT_EQ(reduce_no, 17);
-}
+TEST(IntHashPartitionerTest, PartitionTest) {
+    NodeConfig config;
+    // Only key separator is needed
+    config.set_key_separator("\t");
 
-TEST(Partitioner, IntHashInValid) {
-    TaskInfo task;
-    task.mutable_job()->set_reduce_total(200);
-    task.mutable_job()->set_key_separator(" ");
-    IntHashPartitioner ih_parti(task);
+    Partitioner* partitioner = Partitioner::Get(
+            kIntHashPartitioner, config, dest_num);
+
     std::string key;
-    int reduce_no = ih_parti.Calc("-1 key_123 value456", &key);
-    EXPECT_EQ(key, "key_123");
-    EXPECT_EQ(reduce_no, -1);
-    reduce_no = ih_parti.Calc("", &key);
-    EXPECT_EQ(reduce_no, 0);
+    // Normal case
+    int dest = partitioner->Calc("17 key\tvalue", &key);
+    EXPECT_EQ(key, "key");
+    EXPECT_EQ(dest, 17);
+    EXPECT_EQ(dest, partitioner->Calc("17 key"));
+    // Empty string
+    dest = partitioner->Calc("", &key);
     EXPECT_EQ(key, "");
-    reduce_no = ih_parti.Calc("123 ", &key);
-    EXPECT_EQ(reduce_no, 123);
+    EXPECT_EQ(dest, 0);
+    EXPECT_EQ(dest, partitioner->Calc(""));
+    // No int separator: ' '(even it IS a number)
+    dest = partitioner->Calc("17", &key);
+    EXPECT_EQ(key, "17");
+    EXPECT_EQ(dest, partitioner->HashCode("17") % dest_num);
+    EXPECT_EQ(dest, partitioner->Calc("17"));
+    // Negative column number: use atoi inside, don't care
+    dest = partitioner->Calc("-1 key\tvalue", &key);
+    EXPECT_EQ(key, "key");
+    EXPECT_EQ(dest, -1);
+    EXPECT_EQ(dest, partitioner->Calc("-1 key"));
+    // Column number only
+    dest = partitioner->Calc("17 ", &key);
     EXPECT_EQ(key, "");
-    reduce_no = ih_parti.Calc("123 aaaaaaaaaaaaazzzzzzzz", &key);
-    EXPECT_EQ(reduce_no, 123);
-    EXPECT_EQ(key, "aaaaaaaaaaaaazzzzzzzz");
+    EXPECT_EQ(dest, 17);
+    EXPECT_EQ(dest, partitioner->Calc("17 "));
+    // Invalid int column
+    dest = partitioner->Calc("invalid key\tvalue", &key);
+    EXPECT_EQ(key, "key");
+    EXPECT_EQ(dest, 0);
+    EXPECT_EQ(dest, partitioner->Calc("invalid key"));
+    // More spaces
+    dest = partitioner->Calc("17 key \t value", &key);
+    EXPECT_EQ(key, "key ");
+    EXPECT_EQ(dest, 17);
+    EXPECT_EQ(dest, partitioner->Calc("17 key "));
+    // Empty key
+    dest = partitioner->Calc("17 \tcol2\tcol3", &key);
+    EXPECT_EQ(key, "");
+    EXPECT_EQ(dest, 17);
+    EXPECT_EQ(dest, partitioner->Calc("17 "));
+
+    delete partitioner;
 }
 
 int main(int argc, char* argv[]) {
