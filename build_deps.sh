e710e7ce895b7d7bf2ccbe1575e58df40af69598
Kai-Zhang <cs.zhangkai@outlook.com>
2016-11-29 16:01:06 +0800

    Build boost program options in build deps

--- build_deps.sh
+++ build_deps.sh
@@ -6,7 +6,8 @@ if [ ! -e "./galaxy" ]; then
 		exit -1
 	fi
 fi
-( cd galaxy && git checkout galaxy3 && sh -x ./build.sh )
+( cd galaxy && git checkout galaxy3 && sh -x ./build.sh \
+	&& cd thirdparty/boost_1_57_0 && ./b2 -a link=static )
 if [ $? -ne 0 ]; then
 	echo "build galaxy3 failed"
 	exit -1
